f5afcdf59b7dcb714cb9d68a3690f824bcaf83d0
keechy1231 <76797169+keechy1231@users.noreply.github.com>
2024-12-02 23:03:55 +0000

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
    // Function to load the header HTML content into the page
    window.onload = function() {
     // Fetch the header file and insert it into the 'header-container' div
-        fetch('../header.html')
+        fetch('/header.html')
             .then(response => response.text())
             .then(data => {
                 document.getElementById('header-container').innerHTML = data;
